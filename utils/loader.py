b2e2ae2be30ee1a5dffd5306a00656e2f0f10cb8
Sanvitha-Chopperla <24b01a1225@svecw.edu.in>
2026-05-08 01:56:33 +0530

    clean code for basic ChatBot

--- utils/loader.py
+++ utils/loader.py
@@ -1,13 +1,3 @@
-# from langchain_community.document_loaders import TextLoader
-
-# def load_documents():
-
-#     loader = TextLoader("data/sea_buckthorn.txt")
-
-#     documents = loader.load()
-
-#     return documents
-
 from langchain_community.document_loaders import TextLoader
 
 
--- utils/prompt.py
+++ utils/prompt.py
@@ -1,27 +1,3 @@
-# from langchain.prompts import PromptTemplate
-
-# template = """
-# You are an AI Healthcare Assistant specializing in Sea Buckthorn.
-# Answer the user's question only from the given context.
-
-# If the answer is not available in the context,
-# say:
-# 'I do not have enough medical information.'
-
-# Context:
-# {context}
-
-# Question:
-# {question}
-
-# Answer:
-# """
-
-# PROMPT = PromptTemplate(
-#     template=template,
-#     input_variables=["context", "question"]
-# )
-
 from langchain.prompts import PromptTemplate
 
 template = """
